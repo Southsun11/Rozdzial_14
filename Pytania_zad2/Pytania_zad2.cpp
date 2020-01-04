eafc0e6bfd296619e211ba6717f416191a63d829
dominikmomot@gmail.com <dominikmomot@gmail.com>
2020-01-04 11:32:25 +0100

    Zadania 1-2 Dzial pytania Zadanie 1 Dział zadania

---
+++ Pytania_zad2/Pytania_zad2.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+class Frabjous
+{
+private:
+    char fab[20];
+public:
+    Frabjous(const char* s = "C++") { strncpy_s(fab, s, 20); }
+    virtual void tell() { std::cout << fab; }
+};
+
+class Gloam:private Frabjous
+{
+private:
+    int glip;
+    
+public:
+    Gloam(int g = 0, const char* s = "C++");
+    Gloam(int g, const Frabjous& f);
+    void tell();
+};
+
+Gloam::Gloam(int g, const char* s) :Frabjous(s)
+{
+    this->glip = g;
+}
+
+Gloam::Gloam(int g, const Frabjous& f) : Frabjous(f)
+{
+    this->glip = g;
+}
+
+void Gloam::tell()
+{
+    std::cout << this->glip << std::endl;;
+    Frabjous::tell();
+}
+int main()
+{
+    Gloam g(10, "Hej");
+    g.tell();
+}
+
+
---
+++ Zad1/Zad1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include "Wine.h"
+int main()
+{
+    std::cout << "Podaj nazwe Wina: ";
+    char label[50];
+    std::cin.getline(label, 50);
+    std::cout << "Podaj liczbe rocznikow:";
+    int yrs;
+    while(!(std::cin >> yrs))
+        while (std::cin.get() != '\n')
+            continue;
+    Wine holding(label, yrs);
+    holding.GetBottles();
+    holding.Show();
+    const int YRS = 3;
+    int y[YRS] = { 1993,1995,1998 };
+    int b[YRS] = { 48,60,72 };
+    Wine more("Gushing Grape Red", YRS, y, b);
+    more.Show();
+    std::cout << "Łaczna liczba butelek wina: " << more.Label() << "\t" << more.Sum() << std::endl;
+}
