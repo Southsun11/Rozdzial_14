dd08f07de099d95a3c2c6673e274085bb38ca764
dominikmomot@gmail.com <dominikmomot@gmail.com>
2020-01-04 11:46:45 +0100

    Ponowny commit zadań

---
+++ Zad1/Wine.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <valarray>
+
+template <typename T1, typename T2>
+class Pair
+{
+private:
+	T1 a;
+	T2 b;
+public:
+	T1& first();
+	T2& second();
+	T1 first() const { return a; }
+	T2 second() const { return b; }
+	Pair(const T1& aval, const T2& bval) :a(aval), b(bval) {}
+	Pair() {}
+};
+
+template <typename T1, typename T2>
+T1& Pair<T1, T2>::first()
+{
+	return a;
+}
+
+template <typename T1, typename T2>
+T2& Pair<T1, T2>::second()
+{
+	return b;
+}
+
+
+
+class Wine
+{
+private:
+	std::string m_name;
+	Pair<std::valarray<int>, std::valarray<int>> m_date;
+public:
+	Wine(std::string name = "brak", int year = 0); 
+	Wine(const char* name, int year);
+	Wine(const char* name,int y, int year[], int bottles[]);
+	void GetBottles();
+	void Show();
+	int Sum();
+	std::string Label() { return m_name; }
+};
+
+
